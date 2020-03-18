24b8deb440e07b8db2a5b0e830d272e991481d3e
Your Name <you@example.com>
2020-03-18 10:15:14 +0100

    suppression paramètre inutile pour le kafkaListener

--- src/main/java/com/example/MyKafkaListener.java
+++ src/main/java/com/example/MyKafkaListener.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MyKafkaListener {
-    @KafkaListener(topics = "topic1", containerFactory = "kafkaListenerContainerFactory")
+    @KafkaListener(topics = "topic1")
     public void listen(Payment payment) {
         System.out.println("coucou, message reçu: " + payment.toString());
     }
